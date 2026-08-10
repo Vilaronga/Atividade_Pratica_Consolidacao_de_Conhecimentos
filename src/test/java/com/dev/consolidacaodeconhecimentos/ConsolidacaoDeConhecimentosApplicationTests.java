c91d9194ef3c73b5078500c076b5997950a06d0d
Lucas Vilaronga <lucasvilaronga@gmail.com>
2026-08-10 16:50:33 -0300

    feat(projeto): estrutura base do projeto

---
+++ src/test/java/com/dev/consolidacaodeconhecimentos/ConsolidacaoDeConhecimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.consolidacaodeconhecimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsolidacaoDeConhecimentosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
