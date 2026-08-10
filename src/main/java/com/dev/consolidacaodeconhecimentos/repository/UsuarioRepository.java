6041105b63fa8a0461d4bb635432569e538009ec
Lucas Vilaronga <lucasvilaronga@gmail.com>
2026-08-10 17:57:10 -0300

    feat: adiciona repository referente à entidade Usuario

---
+++ src/main/java/com/dev/consolidacaodeconhecimentos/repository/UsuarioRepository.java
@@ -0,0 +1,24 @@
+package com.dev.consolidacaodeconhecimentos.repository;
+
+import com.dev.consolidacaodeconhecimentos.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findAllByNome(String nome);
+
+    Optional<Usuario> findByEmail(String email);
+
+    Optional<Usuario> findByCpf(String cpf);
+
+    List<Usuario> findByTelefone(String telefone);
+
+    List<Usuario> findAllByDataNascimento(LocalDate dataNascimento);
+
+    List<Usuario> findAllByDataCadastro(LocalDate dataCadastro);
+}
