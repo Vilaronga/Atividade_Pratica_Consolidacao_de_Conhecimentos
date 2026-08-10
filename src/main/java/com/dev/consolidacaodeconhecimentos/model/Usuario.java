165976bf8d198c7a5ea5aba29ec5deaed561944f
Lucas Vilaronga <lucasvilaronga@gmail.com>
2026-08-10 17:28:09 -0300

    feat: adiciona model de Usuário com atributos e anotações JPA

---
+++ src/main/java/com/dev/consolidacaodeconhecimentos/model/Usuario.java
@@ -0,0 +1,35 @@
+package com.dev.consolidacaodeconhecimentos.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(nullable = false)
+    private LocalDate dataCadastro;
+}
